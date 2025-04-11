336b3011c7ad9d443a633ac61da5d57c11542897
Francesco Calcavecchia <f18771@eon.com>
2025-04-11 22:26:40 +0200

    add 06

---
+++ 06-dac_complete_data_schema/src/dac_energy_tutorial_06/__init__.py
@@ -0,0 +1,2 @@
+from dac_energy_tutorial_06.load import load  # noqa: F401
+from dac_energy_tutorial_06.schema import Schema  # noqa: F401
---
+++ 06-dac_complete_data_schema/src/dac_energy_tutorial_06/schema.py
@@ -0,0 +1,109 @@
+import pandera as pa
+from pandera.typing import Series
+import pandas as pd
+import logging
+
+
+class Schema(pa.DataFrameModel):
+    source: Series[str] = pa.Field(
+        isin=[
+            "Solid fossil fuels",
+            "Manufactured gases",
+            "Electricity",
+            "Natural gas",
+            "Heat",
+            "Nuclear heat",
+            "Oil and petroleum products (excluding biofuel portion)",
+            "Peat and peat products",
+            "Renewables and biofuels",
+            "Oil shale and oil sands",
+            "Total",
+            "Non-renewable waste",
+        ],
+        nullable=False,
+        alias="siec_name",
+        description="Source of energy",
+    )
+    location: Series[str] = pa.Field(
+        isin=[
+            "AL",
+            "AT",
+            "BA",
+            "BE",
+            "BG",
+            "CY",
+            "CZ",
+            "DE",
+            "DK",
+            "EA19",
+            "EE",
+            "EL",
+            "ES",
+            "EU27_2020",
+            "EU28",
+            "FI",
+            "FR",
+            "GE",
+            "HR",
+            "HU",
+            "IE",
+            "IS",
+            "IT",
+            "LT",
+            "LU",
+            "LV",
+            "MD",
+            "ME",
+            "MK",
+            "MT",
+            "NL",
+            "NO",
+            "PL",
+            "PT",
+            "RO",
+            "RS",
+            "SE",
+            "SI",
+            "SK",
+            "TR",
+            "UA",
+            "UK",
+            "XK",
+        ],
+        nullable=False,
+        alias="geo",
+        description="Location code, either two-digit ISO 3166-1 alpha-2 code or "
+        "'EA19', 'EU27_2020', 'EU28' for the European Union",
+    )
+    year: Series[int] = pa.Field(
+        ge=1990,
+        le=3000,
+        nullable=False,
+        alias="TIME_PERIOD",
+        description="Year of observation",
+    )
+    value_in_gwh: Series[float] = pa.Field(
+        nullable=True,
+        alias="OBS_VALUE",
+        description="Gross available energy in GWh",
+    )
+
+    @pa.dataframe_check
+    def total_ge_other_sources(cls, df: pd.DataFrame) -> bool:
+        df_pivoted = df.pivot(
+            index=[cls.location, cls.year],
+            columns=cls.source,
+            values=cls.value_in_gwh,
+        ).reset_index()
+        check_result = True
+        for col in df[cls.source].unique():
+            if col != "Total":
+                is_total_smaller = df_pivoted[col] > df_pivoted["Total"]
+                if (is_total_smaller).any():
+                    relevant_cols = [cls.location, cls.year, "Total", col]
+                    logging.error(
+                        "Error: {col} is greater than Total in:\n"
+                        f"{df_pivoted[is_total_smaller].loc[:, relevant_cols]}"
+                    )
+                    check_result = False
+        return check_result
